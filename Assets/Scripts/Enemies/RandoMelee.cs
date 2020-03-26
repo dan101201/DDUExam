e1798aaaeab27ad5bf36036fe33106268a70d2b5
Marcus Schebye Skriver <marcus@familienskriver.dk>
2020-03-26 10:03:23 +0100

    Current state of enemy making
    Still early development. Would be nice to know what kind of enemies we want

---
+++ Assets/Scripts/Enemies/RandoMelee.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandoMelee : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    Vector3 savedPlayerTransform;
+
+    GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        savedPlayerTransform = Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 tempPlayerTransform = Player.transform.position;
+        if (tempPlayerTransform != savedPlayerTransform)
+        {
+            savedPlayerTransform = tempPlayerTransform;
+            navMeshAgent.SetDestination(tempPlayerTransform);
+            Debug.Log("Player has moved");
+        }
+        else
+        {
+            Debug.Log("Player hasn't moved");
+        }
+        //Debug.Log(navMeshAgent.remainingDistance);
+        if (navMeshAgent.remainingDistance < 0.5f)
+        {
+            Debug.Log("Close Enough");
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/RandoShooterEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandoShooterEnemy : MonoBehaviour
+{
+    public GameObject Bal;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
