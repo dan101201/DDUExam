4ae38f208e36cf86d51403a00b0fe189023aa7f3
Marcus Schebye Skriver <marcus@familienskriver.dk>
2020-03-26 10:35:22 +0100

    Made it possible to put the roof back on

---
+++ Assets/Scripts/Rooms/Roofreveal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roofreveal : MonoBehaviour
+{
+    MeshRenderer Roof;
+    public bool RoofState;
+
+    void Start()
+    {
+        Roof = GetComponent<MeshRenderer>();
+        RoofState = Roof.enabled;
+    }
+
+    public void ChangeRoofState()
+    {
+        RoofState = !RoofState;
+        Roof.enabled = RoofState;
+    }
+
+    public void ChangeRoofState(bool newState)
+    {
+        RoofState = newState;
+        Roof.enabled = RoofState;
+    }
+}
---
+++ Assets/Scripts/Rooms/Roomreveal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roomreveal : MonoBehaviour
+{
+    public Roofreveal Roof;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Roof.ChangeRoofState();
+        }
+    }
+}
