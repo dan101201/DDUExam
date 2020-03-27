20de434ac495a404afa21b9b6fc98785c207ca51
daniel <daniel@sindrup.dk>
2020-03-28 00:02:03 +0100

    added exploding fireball

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -6,7 +6,6 @@ public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
     public float bulletSpeed;
-    public float TimeTillDeath;
     public float IntershotDelay;
 
     bool canShoot;
@@ -22,7 +21,6 @@ public class Shooting : MonoBehaviour
         {
             GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
             newBullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
-            Destroy(newBullet, TimeTillDeath);
             canShoot = false;
             StartCoroutine(ShootWait());
         }
