05e815264d759261d75d241fceb2fcefea6ee31c
daniel <daniel@sindrup.dk>
2020-04-07 22:08:36 +0200

    added door scripts so doors open to conecting rooms

---
+++ Assets/Scripts/Rooms/Doors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    DungeonGenerationScript generation;
+    GameObject door;
+    Collider temp;
+
+    private void Start()
+    {
+        temp = gameObject.GetComponent<Collider>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            door = other.gameObject;
+        }
+    }
+    private void Update()
+    {
+        generation = DungeonGenerationScript.dungGeneration;
+        if (generation.canSpawnEnemys)
+        {
+            temp.enabled = true;
+            Destroy(door);
+        }
+    }
+}
