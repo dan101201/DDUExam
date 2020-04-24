3a16ea654a3c6e73b49cd670c296c87e1110ae47
Daniel R <daniel.reffeldt@outlook.com>
2020-04-24 09:11:12 +0200

    added fade to universal sound

--- Assets/Scripts/UniversalSound.cs
+++ Assets/Scripts/UniversalSound.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UniversalSound : MonoBehaviour
 {
     public AudioSource audioSource;
+    public float fade;
 
     public void PlayAudio(AudioClip audio) {
         audioSource.clip = audio;
@@ -13,5 +14,25 @@ public class UniversalSound : MonoBehaviour
 
     public void PlayAudio() {
         audioSource.Play();
+    }
+
+    public void FadePlayAudio(AudioClip audio) {
+        var audioSource2 = gameObject.AddComponent<AudioSource>();
+        audioSource2.clip = audio;
+        audioSource2.volume = 0;
+        audioSource2.Play();
+        audioSource2.clip = audio;
+    }
+
+    IEnumerator FadeAudio(AudioSource source) {
+        float tempVolume = audioSource.volume;
+        while (source.volume < tempVolume)
+        {
+            source.volume += fade/100;
+            audioSource.volume -= fade/100;
+            yield return new WaitForSeconds(0.05f);
+        }
+        source.volume = tempVolume;
+        yield return null;
     }
 }
