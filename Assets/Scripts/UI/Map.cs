b5acc8fff776f4f955cdce6517f5f8ad23d4de30
Tadayo <daniel.reffeldt@outlook.com>
2020-04-30 13:25:58 +0200

    boop map

---
+++ Assets/Scripts/UI/Map.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    private Vector3 offset;
+    private Vector3 oldHit;
+    private Vector3 initialOffset;
+    private bool dragging;
+    public float speed;
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragging = true;
+                initialOffset = hit.point - hit.transform.position;
+            } 
+            else if (Input.GetMouseButtonUp(0))
+            {
+                dragging = false;
+            }
+            else if (dragging)
+            {
+                hit.transform.position = hit.point - initialOffset;
+            }
+        }
+        
+    }
+}
