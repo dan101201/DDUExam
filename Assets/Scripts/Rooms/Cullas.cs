ce94c8f6a9d0508c4f682c6e5d4f75953698fcbc
Marcus Schebye Skriver <marcus@familienskriver.dk>
2020-05-01 15:02:37 +0200

    Fixed roomlock pushing the player when it closes the gates

---
+++ Assets/Scripts/Rooms/Cullas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cullas : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+}
