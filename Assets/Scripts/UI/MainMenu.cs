bca606027de94dd6ae13d047e95116bf618b8891
Marcus Schebye Skriver <marcus@familienskriver.dk>
2020-05-07 12:41:55 +0200

    Added a Main Menu
    This is just a rudimentary menu, but it works

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int gameSceneIndex;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneIndex);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
